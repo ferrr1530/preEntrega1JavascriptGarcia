52c887c3f83e9c833a856a181d370222e4421198
ferrr1530 <fernando.cba1530@gmail.com>
2022-11-30 21:54:07 -0300

    preEntrega1JavascriptGarcia

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-let nombreAlumno = parseInt(prompt("ingrese su nombre:"));
-let materia = parseInt(prompt("ingrese el nombre de la materia a consultar:"));
-let primeraNota = parseInt(prompt("nota de su primer parcial"));
-let segundaaNota = parseInt(prompt("nota de su segundo parcial"));
-let notaFinal = parseInt(prompt("nota de final"));
-let promedio = (primeraNota + segundaaNota + notaFinal) / 3;{
-console.log("su promedio final es de :");
+let nombreAlumno = (prompt("ingrese su nombre:"));
+function saludarAlumno (nombreAlumno){
+    alert("hola " + nombreAlumno);
 }
-    if (promedio < 7){
+saludarAlumno(nombreAlumno);
+let materia = (prompt("ingrese el nombre de la materia a consultar:"));
+let primeraNota = parseInt(prompt("nota de su primer parcial"));
+let segundaNota = parseInt(prompt("nota de su segundo parcial"));
+let notaFinal = parseInt(prompt("nota de tu final"));
+
+function calcularPromedio(nota1, nota2, nota3) {
+    return (nota1 + nota2 + nota3) / 3;
+}
+
+let promedioFinal = calcularPromedio(primeraNota, segundaNota, notaFinal);
+
+console.log("su promedio final es de :" + promedioFinal);
+
+if (promedioFinal < 7){
         alert("has reprobado, por favor ingresa tu usuario para ver las fechas de tu recuperatorio");
-    }
-
-
-
-let usuario = (prompt("ingrese su nombre de usuario: (para salir ingrese: salir)"));
+}else {
+    alert("felicitaciones, estás aprobado");
+}
+let usuario = prompt("ingrese su usuario: (luego de escribir el nombre de usuario ingrese: salir)");
 while (usuario != "salir"){
     console.log(usuario);
-    usuario= prompt("ingrese su usuario: (para salir ingrese: salir)");
+    usuario= prompt("ingrese su usuario: (luego de escribir el nombre de usuario ingrese: salir)");
 }
-
 let contraseniaUsuario = parseInt(prompt("ingrese su contraseña del aula virtual;"));
 const contraseniaDelUsuario = 1234;
 for(let i = 0; i < 2; i ++){
